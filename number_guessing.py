3993934fc74a356dbe4a7d2f80f802eb9f839055
Jorge Fuentes <jf7494@gmail.com>
2022-07-19 21:43:15 +0200

    Update number_guessing.py
    Finished version of the project

--- number_guessing.py
+++ number_guessing.py
@@ -1,62 +1,74 @@
 import random
 
-def seleccionar_dificultad():
-    rango_máximo = input("Por favor, selecciona el número máximo sobre el que tiene que elegir el ordenador: ")
-    return rango_máximo 
- 
-def dar_pistas(pistas):
+def control_input(mensaje):
+    i = 0
+    while i == 0:
+        input_simple = input(mensaje)
+        if input_simple.isdigit() == False:
+           print ("Eso no es un número o el número es negativo. Inténtalo de nuevo.")
+        elif int(input_simple) <=0:
+            print ("El número es demasiado pequeño. Prueba con un número mayor de cero.")
+        else:
+            i = 1
+    return int(input_simple)
+
+def dar_pistas(intento,rango_máximo,solución):
+    pista_a_modificar = ""
     if intento== 5:
         if solución //2 == 0:
-            print ("Primera pista: el número es par.")
+            pista_a_modificar = "par"
         else:
-            print ("Primera Pista: el número es impar.")
+            pista_a_modificar = "impar"
+        print ("Primera Pista: el número es {}.".format(pista_a_modificar))
     elif intento== 4:
         if solución //5 == 0:
-            print ("Segunda pista: el número es divisible por 5.")
+            pista_a_modificar = " "
         else:
-            print ("Segunda pista: el número no es divisible por 5.")
+            pista_a_modificar = "no "
+        print ("Segunda pista: el número {}es divisible por 5.".format(pista_a_modificar))
     elif intento== 3:
         mitad = rango_máximo/2
         if solución > mitad:
-            print("Tercera pista: el número es superior a {}.".format(mitad))
+            pista_a_modificar = "superior"
         else:
-            print("Tercera pista: el número es inferior a {}.".format(mitad))
+            pista_a_modificar = "inferior"
+        print ("Tercera pista: el número es {} a {}.".format(pista_a_modificar, round(mitad)))
     elif intento== 2:
         if solución //3 == 0:
-            print ("cuarta pista: el número es divisible entre 3.")
+            pista_a_modificar = " "
         else:
-            print ("Cuarta pista: el número no es divisible entre 3.")
+            pista_a_modificar = "no "
+        print ("Cuarta pista: el número {} divisible entre 3."format(pista_a_modificar))
     elif intento== 1:
         print ("el número al cuadrado es {}.".format(solución*solución))
-        
-                       
-print ("Bienvenido al number_guessing. En este juego tendrás que adivinar el número que ha elegido el ordenador.")
-print ("Tienes cinco intentos. En cada fallo se te dará una pista.")
-
-rango_máximo = int(seleccionar_dificultad())
+ 
+mensaje_inicial = "Por favor, selecciona el número máximo sobre el que tiene que elegir el ordenador: "
+mensaje_standard = "Introduce tu respuesta: "
+rango_máximo = (control_input(mensaje_inicial))
 solución = random.randint(1,rango_máximo)
 input_jugador = 0
-intento = 5
-
+intento = 5   
+                   
+print ("Bienvenido al number_guessing. En este juego tendrás que adivinar el número que ha elegido el ordenador.")
+print ("Tienes cinco intentos. En cada fallo se te dará una pista.")
 print("El ordenador ha elegido el número. ¡Empezamos con la primera pista!")
 
-for i in range(5):
-    dar_pistas(intento)
-    input_jugador = (input("Introduce tu respuesta: "))
+while intento > 0:
+    dar_pistas(intento,rango_máximo,solución)
+    input_jugador = control_input(mensaje_standard)
     if input_jugador == solución:
        print ("¡Felicidades! Has conseguido superar el juego.")
        print ("El número a adivinar era {}.".format(solución))
-       print ("Tu puntuación final es {}".format(5-intento))
-       break
+       print ("Tu puntuación final es {}".format(intento))
     elif intento == 1 and input_jugador != solución:
         print ("Lo siento. Has agotado el número de intentos. Tendrás que empezar de nuevo.")
-        break
     elif intento == 2:
         intento -= 1
         print ("Has fallado. Te quedan {} intento. Añadimos otra pista.".format(intento))
     else:
         intento -= 1
         print ("Has fallado. Te quedan {} intentos. Añadimos otra pista.".format(intento))
+        
         
         
         
